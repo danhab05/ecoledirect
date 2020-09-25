0c8a16b7932971fbf3d1d5eef11d6eae0bfef8e6
Your Name <you@example.com>
2020-09-25 18:35:31 +0200

    Ecoledirect

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ecoledirect',
+    version='1.0',
+    description='With this package you can get the data from the website www.ecoledirect.com.',
+    url='https://github.com/Snowy-27/EcoleDirect',
+    author='Dan Habib',
+    author_email='dan.habib@yahoo.fr',
+    license='unlicense',
+    packages=['ecoledirect'],
+    zip_safe=False
+)
