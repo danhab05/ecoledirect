af680ebde52962a693f1d19f602329fa5b1a26ac
Your Name <you@example.com>
2021-10-31 21:29:44 +0100

    format correct

--- ecoledirect/__init__.py
+++ ecoledirect/__init__.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import time
 from webdriver_manager.chrome import ChromeDriverManager
 import re
+import html
+
 
 class Ecoledirect_bot:
     driver = None
@@ -10,6 +12,7 @@ class Ecoledirect_bot:
     homework = []
     day = []
     dic = {}
+
     def __init__(self, username, password, hidden=True):
         self.username = username
         self.password = password
@@ -22,30 +25,37 @@ class Ecoledirect_bot:
         if self.hidden:
             self.op = webdriver.ChromeOptions()
             self.op.add_argument('headless')
-            self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=self.op)
+            self.driver = webdriver.Chrome(ChromeDriverManager().install(),
+                                           options=self.op)
         else:
             self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.driver.get('https://www.ecoledirecte.com/login')
-        nameInput = self.driver.find_element_by_xpath('/html/body/div[2]/div[1]/div/div/div[1]/div[3]/form/input[1]')
-        passInput = self.driver.find_element_by_xpath('/html/body/div[2]/div[1]/div/div/div[1]/div[3]/form/input[2]')
+        nameInput = self.driver.find_element_by_xpath(
+            '/html/body/div[2]/div[1]/div/div/div[1]/div[3]/form/input[1]')
+        passInput = self.driver.find_element_by_xpath(
+            '/html/body/div[2]/div[1]/div/div/div[1]/div[3]/form/input[2]')
         nameInput.send_keys(self.username)
         passInput.send_keys(self.password)
-        button = self.driver.find_element_by_xpath('/html/body/div[2]/div[1]/div/div/div[1]/div[3]/form/button')
+        button = self.driver.find_element_by_xpath(
+            '/html/body/div[2]/div[1]/div/div/div[1]/div[3]/form/button')
         button.click()
         time.sleep(2)
-
 
     def get_notes(self):
         pass
 
     def get_homework(self):
         time.sleep(1)
-        self.driver.find_element_by_xpath("/html/body/div[2]/div[1]/div/ed-menu[2]/div/a/div").click()
-        self.driver.find_element_by_xpath('/html/body/div[2]/div[1]/div/ed-menu[2]/div/div/ul/li[7]/a').click()
+        self.driver.find_element_by_xpath(
+            "/html/body/div[2]/div[1]/div/ed-menu[2]/div/a/div").click()
+        self.driver.find_element_by_xpath(
+            '/html/body/div[2]/div[1]/div/ed-menu[2]/div/div/ul/li[7]/a'
+        ).click()
         # SEMAINE
         time.sleep(1)
         self.driver.find_element_by_xpath(
-            '/html/body/div[2]/div[2]/div[2]/cahier-de-texte/div/div[1]/div[2]/div[2]/ul/li[10]/a').click()
+            '/html/body/div[2]/div[2]/div[2]/cahier-de-texte/div/div[1]/div[2]/div[2]/ul/li[10]/a'
+        ).click()
         time.sleep(2)
         page = self.driver.page_source
         soup = BeautifulSoup(page, 'lxml')
@@ -59,9 +69,8 @@ class Ecoledirect_bot:
             currentday = h.text
             self.day.append(h.text)
             for txt in div.find_all('div', {'class': 'col-md-9'}):
-                self.homework.append(txt.text)
-                print(txt.text)
-                currenthome = txt.text
+                currenthome = html.unescape(
+                    txt.text.replace('\\n', "\n").split("Donné le")[0])
             for h3 in div.find_all('h3', {'class': ""}):
                 if h3.text.isupper():
                     self.matiere.append(h3.text)
@@ -69,8 +78,8 @@ class Ecoledirect_bot:
 
         i = 0
 
-
         list = []
+        return self.dic
 
     def close(self):
         self.driver.close()
